5645ba38c9a1652c92aa2bbe18bbd6aa01a70ea7
kabistarvn <157883250+kabistarvn@users.noreply.github.com>
2025-06-23 05:02:02 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold">Chào mừng đến với Shop Tài Khoản</h1>
+      <p>Vui lòng <a href="/login" className="text-blue-600 underline">đăng nhập</a> để tiếp tục</p>
+    </div>
+  );
+}
